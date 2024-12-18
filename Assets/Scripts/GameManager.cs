96f31ca592641db07dbd859a8e6857bdce86b4e0
ArthurCisotto <arthurcisottom@gmail.com>
2024-12-18 02:18:39 -0300

    feat: results screen

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -15,6 +15,12 @@ public class GameManager : MonoBehaviour
     [SerializeField] private int currentMultiplier;
     [SerializeField] private int multiplierTracker;
     [SerializeField] private int[] multiplierThresholds;
+
+    [SerializeField] private int totalNotes;
+    [SerializeField] private int normalHits, goodHits, perfectHits, missedHits;
+
+    [SerializeField] private GameObject resultsScreen;
+    [SerializeField] private Text percentHitText, normalsText, goodsText, perfectsText, missesText, rankText, finalScoreText;
     public static GameManager instance;
 
     void Start()
@@ -22,6 +28,7 @@ public class GameManager : MonoBehaviour
         instance = this;
         scoreText.text = "Score: 0";
         currentMultiplier = 1;
+        totalNotes = FindObjectsOfType<NoteObject>().Length;
     }
 
     void Update()
@@ -33,6 +40,44 @@ public class GameManager : MonoBehaviour
                 startPlaying = true;
                 beatScroller.hasStarted = true;
                 audioSource.Play();
+            }
+        } else
+        {
+            if (!audioSource.isPlaying && !resultsScreen.activeInHierarchy)
+            {
+                resultsScreen.SetActive(true);
+                normalsText.text = "" + normalHits;
+                goodsText.text = "" + goodHits;
+                perfectsText.text = "" + perfectHits;
+                missesText.text = "" + missedHits;
+                int totalHit = normalHits + goodHits + perfectHits;
+                float percentHit = (float)totalHit / (float)totalNotes * 100f;
+                percentHitText.text = percentHit.ToString("F1") + "%";
+
+                string rankVal = "F";
+                if (percentHit > 40)
+                {
+                    rankVal = "D";
+                }
+                if (percentHit > 55)
+                {
+                    rankVal = "C";
+                }
+                if (percentHit > 70)
+                {
+                    rankVal = "B";
+                }
+                if (percentHit > 85)
+                {
+                    rankVal = "A";
+                }
+                if (percentHit > 95)
+                {
+                    rankVal = "S";
+                }
+                rankText.text = rankVal;
+
+                finalScoreText.text = "" + currentScore;
             }
         }
     }
@@ -60,24 +105,28 @@ public class GameManager : MonoBehaviour
         currentMultiplier = 1;
         multiplierTracker = 0;
         multiText.text = "Multiplier: x" + currentMultiplier;
+        missedHits++;
     }
 
     public void NormalHit()
     {
         currentScore += scorePerNote * currentMultiplier;
         NoteHit();
+        normalHits++;
     }
 
     public void GoodHit()
     {
         currentScore += scorePerGoodNote * currentMultiplier;
         NoteHit();
+        goodHits++;
     }
 
     public void PerfectHit()
     {
         currentScore += scorePerPerfectNote * currentMultiplier;
         NoteHit();
+        perfectHits++;
     }
 
 }
