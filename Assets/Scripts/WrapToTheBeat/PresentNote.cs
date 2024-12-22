b52419e7a28319c75af60d014d98a09fc616fdf4
ArthurCisotto <arthurcisottom@gmail.com>
2024-12-22 18:51:54 -0300

    feat: basic minigame mechanics done

---
+++ Assets/Scripts/WrapToTheBeat/PresentNote.cs
@@ -0,0 +1,79 @@
+using UnityEngine;
+
+public class PresentNote : NoteObject
+{
+    [Header("Present Visuals")]
+    [SerializeField] private GameObject unwrappedBox;
+    [SerializeField] private GameObject perfectWrappedPresent;
+    [SerializeField] private GameObject messyWrappedPresent;
+    
+    protected override void OnNoteProcessed()
+    {
+        // Desativa a caixa não embrulhada
+        if (unwrappedBox != null)
+        {
+            unwrappedBox.SetActive(false);
+        }
+        
+        // Ativa o presente apropriado baseado na precisão do hit
+        switch (lastHitAccuracy)
+        {
+            case HitAccuracy.Perfect:
+                if (perfectWrappedPresent != null)
+                {
+                    perfectWrappedPresent.SetActive(true);
+                }
+                break;
+                
+            case HitAccuracy.Early:
+            case HitAccuracy.Late:
+                if (messyWrappedPresent != null)
+                {
+                    messyWrappedPresent.SetActive(true);
+                }
+                break;
+                
+            default:
+                // No caso de miss, deixa a caixa não embrulhada
+                if (unwrappedBox != null)
+                {
+                    unwrappedBox.SetActive(true);
+                }
+                break;
+        }
+    }
+
+    public void ResetPresent()
+    {
+        if (unwrappedBox != null) unwrappedBox.SetActive(true);
+        if (perfectWrappedPresent != null) perfectWrappedPresent.SetActive(false);
+        if (messyWrappedPresent != null) messyWrappedPresent.SetActive(false);
+        
+        ResetNote();
+    }
+    
+    protected override void SpawnEffect(GameObject effectPrefab)
+    {
+        // Ajusta a posição do efeito para aparecer um pouco acima do presente
+        Vector3 effectPosition = transform.position + Vector3.up * 2f;
+        Instantiate(effectPrefab, effectPosition, Quaternion.LookRotation(Camera.main.transform.forward));
+    }
+
+        private void OnTriggerEnter(Collider other)
+    {
+        if (other.CompareTag("Activator"))
+        {
+            canBePressed = true;
+        }
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        if (!wasPressed && other.CompareTag("Activator") && gameObject.activeSelf)
+        {
+            canBePressed = false;
+            lastHitAccuracy = HitAccuracy.Miss;
+            OnNoteProcessed();
+        }
+    }
+}
